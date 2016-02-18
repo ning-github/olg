5fc25746855c96e5f23c22402dfc94238aee72bd
Ning Xia <nxiamail@gmail.com>
2016-02-17 20:58:16 -0800

    Determine and log line number of caller

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 module.exports = wog;
 
 function wog (logMe) {
-    console.log(logMe, " ---logged from " + __filename);
+    var callerFrame = new Error().stack.split("\n")[2];
+    var lineNumIndex = callerFrame.indexOf(__filename);
+    var dirtyLine = callerFrame.slice(lineNumIndex+__filename.length+1);
+    var chopIndex = dirtyLine.indexOf(":");
+    var lineNum = dirtyLine.slice(0, chopIndex);
+
+    console.log(logMe + " ---logged from " + __filename + " at line " + lineNum);
 }
